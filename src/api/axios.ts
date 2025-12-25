8250cff102f80333773072031c1891fa2c9aa746
Badre Abouanane <badrezoopro@gmail.com>
2025-12-25 19:13:17 +0100

    fix axiosapi

--- src/api/axios.ts
+++ src/api/axios.ts
@@ -1,8 +1,12 @@
-import axios from "axios";
+import axios from 'axios';
 
 export const api = axios.create({
-   baseURL: import.meta.env.VITE_API_URL,
+  baseURL: import.meta.env.VITE_API_URL,
+  headers: {
+    'Content-Type': 'application/json',
+  },
 });
+
 
 // ✅ ajoute automatiquement le token sur chaque requête
 api.interceptors.request.use((config) => {
